6a70e729562ffbff24d766ddf0029cb12c7411c5
devhong96 <dev.hong96@gmail.com>
2025-02-08 22:46:17 +0900

    first commit

---
+++ src/main/java/com/scheduler/ebookdiscoveryservice/SchedulerDiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.scheduler.ebookdiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SchedulerDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerDiscoveryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scheduler/ebookdiscoveryservice/SchedulerDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheduler.ebookdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
